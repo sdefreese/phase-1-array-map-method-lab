09e2cd5b088baf45824ea9dd8f9d096855db78ac
sdefreese <samanthadefreese1@gmail.com>
2021-09-14 17:39:31 -0700

    commit tree 210d146096310297c80238269d6bff4425707ded

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  const tutorialSplitString = tutorials.map((string) => string.split(" "));
+  const capitalizedWords = [];
+  for (const element of tutorialSplitString) {
+    const change = element
+      .map((word) => word[0].toUpperCase() + word.substring(1))
+      .join(" ");
+    capitalizedWords.push(change);
+  }
+  return capitalizedWords;
+};
